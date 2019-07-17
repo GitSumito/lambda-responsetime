46274f92d99dca0b9798e2a37dd0976711df70c0
sumito.tsukada <sumito.tsukada@rarejob.co.jp>
2019-07-17 16:38:04 +0900

    add cloudwatch

--- function.sh
+++ function.sh
@@ -1,6 +1,5 @@
 function handler () {
 	res=$(curl -kL 'https://tsukada.sumito.jp' -o /dev/null -w "%{time_total}" 2> /dev/null)
-	s=`echo "$res * 1000" | bc` 
-	echo ${s%.*} 1>&2;
+	aws cloudwatch put-metric-data --metric-name responseTime --namespace responseTime --value $res --dimensions Name=Time,Value=second 
 }
 
